48c0a3787368543061b10e4d1c7d8e6b3fa1cf71
Brad Hopper <bhopper@pdx.edu>
2019-07-13 20:17:25 -0700

    ggez commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use ggez::*;
+
+pub fn main() {
+  let state = &mut State { };
+
+  let c = conf::Conf::new();
+  let (ref mut ctx, ref mut event_loop) = ContextBuilder::new("Rustworld", "bhopper")
+    .conf(c)
+    .build()
+    .unwrap();
+
+  event::run(ctx, event_loop, state).unwrap();
+}
+
+struct State{}
+
+// https://github.com/ggez/ggez/blob/master/docs/guides/HelloGgez.md
+impl ggez::event::EventHandler for State {
+  fn update(&mut self, ctx: &mut Context) -> GameResult<()> {
+    Ok(())
+  }
+  fn draw(&mut self, ctx:&mut Context) -> GameResult<()> {
+    Ok(())
+  }
 }
