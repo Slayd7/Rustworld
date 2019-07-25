58587526657fc618772afceeb8f6015ce8c399d4
Brad Hopper <brad.chopper@gmail.com>
2019-07-24 17:15:07 -0700

    combining into one file... no need to overcomplicate for now

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,6 @@ mod rw_tile_lib;
 mod rw_import;
 
 struct State {
-  shapes: Vec<Shape>,
-}
-
-enum Shape {
-  Circle(graphics::Point2, f32),
-  Rectangle(graphics::Rect),
 }
 
 impl ggez::event::EventHandler for State {
@@ -23,46 +17,83 @@ impl ggez::event::EventHandler for State {
   }
   fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
     graphics::clear(ctx);
-    for shape in &self.shapes {
-      match shape {
-        &Shape::Rectangle(rect) => {
-          graphics::rectangle(ctx, graphics::DrawMode::Fill, rect).unwrap();
-        }
-        &Shape::Circle(origin, radius) => {
-          graphics::circle(ctx, graphics::DrawMode::Fill, origin, radius, 0.1).unwrap();
-        }
-      }
-    }
     graphics::present(ctx);
     Ok(())
   }
 }
 
-fn main() {
-  let mut shapes = Vec::new();
-  for _ in 0..8 {
-    if thread_rng().gen_range(0, 2) % 2 == 0 {
-      shapes.push(Shape::Rectangle(ggez::graphics::Rect::new(
-        thread_rng().gen_range(0.0, 800.0),
-        thread_rng().gen_range(0.0, 600.0),
-        thread_rng().gen_range(0.0, 800.0),
-        thread_rng().gen_range(0.0, 600.0),
-      )));
-    } else {
-      shapes.push(Shape::Circle(
-        ggez::graphics::Point2::new(
-          thread_rng().gen_range(0.0, 800.0),
-          thread_rng().gen_range(0.0, 600.0),
-      ),
-      thread_rng().gen_range(0.0, 300.0),
-    
-  ));
-    }
-  }
-
-
-  let state = &mut State { shapes: shapes };
+fn main() 
+{
+  let state = &mut State { };
   let c = conf::Conf::new();
   let ctx = &mut Context::load_from_conf("title", "author", c).unwrap();
-  event::run(ctx, state).unwrap();
+  let lib = &mut rw_tile_lib::Lookup::new().unwrap();
+
+  if rw_import::import_tiles(ctx, lib) {
+    event::run(ctx, state).unwrap();
+  }
 }
+
+pub struct Lookup {
+  pub floors: Vec<Tile>,
+  pub walls: Vec<Tile>,
+  pub terrain: Vec<Tile>,
+  pub objects: Vec<Tile>,
+}
+
+impl Lookup {
+  pub fn new() -> GameResult<Lookup> {
+    
+    Ok(Lookup {
+      floors:  Vec::new(),
+      walls:   Vec::new(),
+      terrain: Vec::new(),
+      objects: Vec::new()
+    })
+  }
+}
+
+pub struct Tile {
+  pub name: ::std::string::String,
+  pub description: ::std::string::String,
+
+  pub sprite: ::ggez::graphics::Image,
+//  stats: Tile_stats,
+}
+
+impl Tile {
+  pub fn new(name: String, description: String, sprite: graphics::Image) -> Tile {
+    Tile {
+      name: name,
+      description: description,
+      sprite: sprite,
+    }
+  }
+}
+
+struct Tile_stats {
+// TODO
+}
+
+fn visit_dirs(dir: &Path) -> GameResult<Vec<Tile>>{
+  if dir.is_dir() {
+    for entry in fs::read_dir(dir)? {
+      let entry = entry?;
+      let path = entry.path();
+      println!("{}", path.display());
+
+    }
+  }
+  Ok()
+
+}
+
+pub fn import_tiles(ctx: &mut ggez::Context, mut lib: &Lookup) -> bool
+{
+  let resourcesloc = ctx.filesystem.get_resources_dir();
+  visit_dirs(resourcesloc);
+
+
+  true
+}
+
